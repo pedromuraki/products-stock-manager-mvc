cdc2f806a77d14c42aef42a17c354064fa33778a
Pedro Muraki <pedromuraki@gmail.com>
2019-04-23 15:10:36 -0300

    Created client directory

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -3,4 +3,4 @@ import '../scss/index.scss'
 // const importAll = r => r.keys().forEach(r);
 // importAll(require.context('../img/', true, /\.(jpe?g|png|gif)$/));
 
-import { toggleClass, addClass, removeClass, nodelistToArray } from './helpers'
+import { toggleClass, addClass, removeClass, nodelistToArray } from './helpers/helpers'
